3e5b4b600c68807091ff37bd71ad9f3faee36f68
gitluffie <gitluffie@gmail.com>
2025-05-08 06:22:29 +0000

    May 8 Thursday JS 1st Commit

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,75 @@
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+// let someNumber = 222
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+// let str1 = "Good Morning"
+// let str2 = " Hitesh"
+
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// // console.log("1"+2+2); dangerous zone 
+// console.log(1+2+"2");
+
+let gameCounter = 100
+++gameCounter;
+++gameCounter;
+console.log(gameCounter);
+
+
+let x = 3;
+let y = x++;
+console.log(x, y);
+
+
+let a = 5;
+let b = ++a;
+console.log(a, b);
+
+
+let num = 10;
+num++;
+++num;
+console.log(num);
