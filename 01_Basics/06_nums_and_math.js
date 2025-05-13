5c711f738675bdb5dd7d0c03a4759227364d7a80
gitluffie <gitluffie@gmail.com>
2025-05-13 02:31:25 +0000

    math shikchi

--- 01_Basics/06_nums_and_math.js
+++ 01_Basics/06_nums_and_math.js
@@ -1,5 +1,29 @@
 const score = 40
-
+console.log(score);
+console.log(typeof score);
 
 const balance = new Number (100)
 console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+
+
+const othernumber = 123.8966
+
+console.log(othernumber.toPrecision(3));
+
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++maths ++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+
+
+
