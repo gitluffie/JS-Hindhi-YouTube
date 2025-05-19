36ce7a73a9a31aacd72ab9d37bbc2c053d519907
gitluffie <gitluffie@gmail.com>
2025-05-19 02:15:15 +0000

    final date from chat gpt

--- 01_Basics/07_datesinJs.js
+++ 01_Basics/07_datesinJs.js
@@ -1,3 +1,11 @@
-//date
-let mydate = new Date()
-console.log(mydate.toString());
+// Create a new Date object
+const newDate = new Date();
+
+// Log the full date and time
+console.log("Full date and time:", newDate.toString());
+
+// Get and log the weekday name
+const weekday = newDate.toLocaleString('default', {
+    weekday: "long"
+});
+console.log("Today is:", weekday);
