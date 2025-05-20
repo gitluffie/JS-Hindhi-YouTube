9557d58cd39f5ec135e502978ce94e814e924d05
gitluffie <gitluffie@gmail.com>
2025-05-20 02:36:22 +0000

    arry is deficult

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,24 @@
+//array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[1]);
+
+// arraly mehods
+
+
+myArr.push(6)
+myArr.push(7)
+console.log(myArr)
+
+console.log(myArr.includes(9));
+console.log(myArr.indexOf(9));
+console.log(myArr.indexOf(3));
+
+const newArr = myArr.join
+
+
+console.log(myArr);
+console.log(typeof newArr);
