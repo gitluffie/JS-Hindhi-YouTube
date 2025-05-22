b1526a18b89eb1732aeec7aa82ecfbe8d9059051
gitluffie <gitluffie@gmail.com>
2025-05-22 02:24:29 +0000

    print korte hobe aro

---
+++ 02_Basics/03_object.js
@@ -0,0 +1,15 @@
+// singleton
+
+// obj litterals
+
+// Object.create 
+const JsUser = {
+    name: "Hitesh",
+    age: 18,
+    location: "jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+};
+
+console.log(JsUser["email"]);
