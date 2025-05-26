93e4a807cdc59eb8fbfbb01271c8234b901c067e
gitluffie <gitluffie@gmail.com>
2025-05-26 02:31:45 +0000

    s

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,24 @@
+
+
+
+function sayMyname(){
+
+    console.log("H");
+console.log("r");
+console.log("e");
+console.log("t");
+
+
+}
+
+sayMyname()
+
+
+
+function addTwoNumbers(number1, number2){
+
+    console.log(number1 + number2 );
+    
+}
+
+addTwoNumbers(3, 18888) 
