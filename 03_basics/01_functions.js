342e1d90ee59988246fc93a9a56699a4d429e128
gitluffie <gitluffie@gmail.com>
2025-05-27 03:26:08 +0000

    againg

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,3 +22,31 @@ function addTwoNumbers(number1, number2){
 }
 
 addTwoNumbers(3, 18888) 
+
+
+function loginUserMessage(username = "sam") {
+    if (!username) {
+        console.log("Please enter a username");
+        return;
+    }
+    return `${username} just logged in`;
+}
+
+console.log(loginUserMessage(""));
+
+
+function loginUserMessage(username) {
+    if (!username) {
+        username = "sam"; // Set default if empty or falsy
+    }
+    return `${username} just logged in`;
+}
+
+console.log(loginUserMessage("hitesh"));  // Outputs: "sam just logged in"
+
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 600 , 2000,))
