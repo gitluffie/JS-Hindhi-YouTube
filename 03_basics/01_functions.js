c69344fa4049c5fccee63549c8ea28dcbaceebb1
gitluffie <gitluffie@gmail.com>
2025-05-29 05:51:02 +0000

    fun

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -42,11 +42,38 @@ function loginUserMessage(username) {
     return `${username} just logged in`;
 }
 
-console.log(loginUserMessage("hitesh"));  // Outputs: "sam just logged in"
+// console.log(loginUserMessage("hitesh"));  // Outputs: "sam just logged in"
 
 
 function calculateCartPrice(...num1){
     return num1
 }
 
-console.log(calculateCartPrice(200, 400, 600 , 2000,))
+// console.log(calculateCartPrice(200, 400, 600 , 2000,))
+
+const user = {
+    username: "hitesh",
+    price: 199
+};
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is $${anyobject.price}`);
+}
+
+// handleObject(user); // Example function call
+
+handleObject (
+    {
+        username:"sam" ,
+        price: 399
+    }
+)
+
+
+const mynewArray = [200, 300, 500, 20];
+
+function returnSecondValue(getArray) {
+    return getArray[3];
+}
+
+console.log(returnSecondValue(mynewArray));
